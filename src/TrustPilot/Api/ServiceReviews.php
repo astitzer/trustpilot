2282de11b5e49155e53508b0f68bc0fdea5b5003
astitzer <astitzer@gmail.com>
2025-01-07 16:23:55 -0500

    Update ServiceReviews.php

--- src/TrustPilot/Api/ServiceReviews.php
+++ src/TrustPilot/Api/ServiceReviews.php
@@ -19,5 +19,25 @@ use TrustPilot\TrustPilot;
 class ServiceReviews extends AbstractApi{
     
     // Incomplete
-
+    /**
+     * Get private service reviews
+     * https://developers.trustpilot.com/business-units-api#get-a-business-unit's-reviews
+     *
+     *
+     * @param  string, array
+     * @return \stdClass
+     */
+    public function getPrivateReviews($businessUnitId, $data)
+    {
+        return json_decode(
+            $this->api->get('private/business-units/'. $businessUnitId .'/reviews',
+                ['query' =>
+                    [
+                       'page' => $data['page'],
+                       'perPage' => $data['perPage'],
+                       'language' => $data['language'],
+                    ]
+                ]
+            ));
+    }
 }
